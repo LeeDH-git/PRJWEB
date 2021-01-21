da54b3d3d0a84bdd39cc811540ea9f898b6284c3
LeeDH-git <djrl02@naver.com>
2021-01-21 17:23:29 +0900

    1 / 21 commit

--- src/main/java/org/leedh/controller/HomeController.java
+++ src/main/java/org/leedh/controller/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
     /**
      * Simply selects the home view to render by returning its name.
      */
-    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String login(Locale locale, Model model) {
         logger.info("Welcome home! The client locale is {}.", locale);
 
