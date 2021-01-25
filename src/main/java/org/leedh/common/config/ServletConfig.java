e0c250e346d3d59a40673e42a7898bc83c8460cb
LeeDH-git <djrl02@naver.com>
2021-01-25 15:44:46 +0900

    1 / 25 commit

--- src/main/java/org/leedh/common/config/ServletConfig.java
+++ src/main/java/org/leedh/common/config/ServletConfig.java
@@ -1,42 +1,42 @@
-package org.leedh.common.config;
-
-import org.leedh.common.interceptor.LoginInterceptor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.*;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
-
-@EnableWebMvc
-@ComponentScan(basePackages = {"org.leedh.user.controller"})
-public class ServletConfig implements WebMvcConfigurer {
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setViewClass(JstlView.class);
-        bean.setPrefix("/WEB-INF/views/");
-        bean.setSuffix(".jsp");
-        registry.viewResolver(bean);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-    }
-
-    @Bean
-    public MultipartResolver multipartResolver() {
-        return new StandardServletMultipartResolver();
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor())
-                .addPathPatterns("/user/login");
-    }
-}
+//package org.leedh.common.config;
+//
+//import org.leedh.common.interceptor.LoginInterceptor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.web.multipart.MultipartResolver;
+//import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+//import org.springframework.web.servlet.config.annotation.*;
+//import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//import org.springframework.web.servlet.view.JstlView;
+//
+//@EnableWebMvc
+//@ComponentScan(basePackages = {"org.leedh.user.controller"})
+//public class ServletConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void configureViewResolvers(ViewResolverRegistry registry) {
+//
+//        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+//        bean.setViewClass(JstlView.class);
+//        bean.setPrefix("/WEB-INF/views/");
+//        bean.setSuffix(".jsp");
+//        registry.viewResolver(bean);
+//    }
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//
+//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//    }
+//
+//    @Bean
+//    public MultipartResolver multipartResolver() {
+//        return new StandardServletMultipartResolver();
+//    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new LoginInterceptor())
+//                .addPathPatterns("/user/login");
+//    }
+//}
